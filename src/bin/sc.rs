f0133d170c375f93001304b705c8b579339b3308
Domas Monkus <domas@monkus.lt>
2017-05-12 22:31:16 +0300

    Stack implemented.

--- src/bin/sc.rs
+++ src/bin/sc.rs
@@ -3,5 +3,7 @@ extern crate sc;
 use sc::stack;
 
 fn main() {
+    let s = stack::Stack::<i32>::new();
+    
     println!("{}", stack::GREETING);
 }
--- src/stack.rs
+++ src/stack.rs
@@ -1,2 +1,37 @@
 pub const GREETING: &'static str = "well hello!";
 
+pub struct Stack<T> {
+    S: Option<Box<StackElement<T>>>,
+}
+
+struct StackElement<T> {
+    value: T,
+    next: Stack<T>,
+}
+
+impl<T> Stack<T> {
+
+    pub fn new() -> Stack<T> {
+        Stack{S: None}
+    }
+
+    pub fn push(self, elem: T) -> Stack<T> {
+        Stack{
+            S: Some(Box::new(
+            StackElement{
+                value: elem,
+                next: self,
+            })),
+        }
+    }
+
+    pub fn pop(self) -> (Stack<T>, Option<T>) {
+        match self {
+            Stack{S: None} => (self, None),
+            Stack{S: Some(b)} => {
+                let el = *b;
+                (el.next, Some(el.value))
+            }
+        }
+    }
+}
